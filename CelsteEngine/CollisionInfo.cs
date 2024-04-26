82135468c6acbe347fa0bd951acc5ee2220aa8d1
NitroShake <63812175+NitroShake@users.noreply.github.com>
2024-04-26 22:14:32 +0100

    Added CollisionInfo (will it be used? [hint:no])

---
+++ CelsteEngine/CollisionInfo.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsteEngine
+{
+    public struct CollisionInfo
+    {
+        Vector3 point;
+        Vector3 normal;
+
+        public CollisionInfo(Vector3 point, Vector3 normal)
+        {
+            this.point = point;
+            this.normal = normal;
+        }
+    }
+}
